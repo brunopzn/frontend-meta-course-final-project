080e6c8b89fa80f1f46970ba5d0e8f56f39ebb3d
Brunopzn <222485171+brunopzn@users.noreply.github.com>
2026-01-27 10:25:37 -0300

    conteúdo ajustado para para mobile. Adicionado menu Hamburguer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <button>Reserve a Table</button>
+                    <button className={style.btnHero}>Reserve a Table</button>
                 </div>
                 <div className={style.divImg}>
                     <img src={heroImg} className={style.heroImg} alt="Food Image"/>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,51 @@
 import styles from './Nav.module.css'
+import hamburger from '../assets/hamburgerIcon.svg'
+import { useState } from 'react';
 
 function Nav() {
+    const [menuAberto, setMenuAberto] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuAberto(!menuAberto)
+    }
+
     return (
         <nav className={styles.navbar}>
-            <ul>
-                <li>
-                    <a href="#" className={styles.link}>
-                        HOME
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className={styles.link}>
-                        ABOUT
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className={styles.link}>
-                        MENU
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className={styles.link}>
-                        RESERVATIONS
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className={styles.link}>
-                        ORDER ONLINE
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className={styles.link}>
-                        LOGIN
-                    </a>
-                </li>
-            </ul>
+            <button onClick={toggleMenu} className={styles.hamburguerBtn}><img src={hamburger} className={styles.hamburguerImg} alt="Hamburger menu icon"  /></button>
+            <div className={menuAberto ? styles.navbarMobile : styles.navbarMobileHide}>
+                <ul>
+                    <li>
+                        <a href="#" className={styles.link}>
+                            HOME
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={styles.link}>
+                            ABOUT
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={styles.link}>
+                            MENU
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={styles.link}>
+                            RESERVATIONS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={styles.link}>
+                            ORDER ONLINE
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={styles.link}>
+                            LOGIN
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
