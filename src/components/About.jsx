51853e3119da739fd9bf8f1e683d7d0e281d1f1d
Brunopzn <222485171+brunopzn@users.noreply.github.com>
2026-01-28 10:20:19 -0300

    Atualização da responsividade da section  testimonials. Conclusão da section About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,30 @@
 import style from './About.module.css'
+import restaurantChefB from '../assets/restaurantchefB.jpg'
+import restaurant from '../assets/restaurant.jpg'
 function About() {
     return (
         <section className={style.about}>
-            <h2>about</h2>
+            <div className={style.aboutContainer}>
+                <div className={style.titleAndDescriptonBox}>
+                    <h1 className={style.title}>
+                        Little Lemon
+                    </h1>
+                    <h2 className={style.subTitle}>
+                        Chicago
+                    </h2>
+                    <p className={style.description}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum temporibus pariatur aliquam mollitia hic ullam officia reprehenderit magni, eius necessitatibus recusandae. Ut, aliquam sapiente? Aliquid quo molestias fugiat reiciendis suscipit?
+                    </p>
+                </div>
+                <div className={style.imgBox}>
+                        <div className={style.firstImgBox}>
+                            <img className={style.firstImg} src={restaurant} alt="restaurant image" />
+                        </div>
+                        <div className={style.secondImgBox}>
+                            <img className={style.secondImg} src={restaurantChefB} alt="restaurant Chef image" />
+                        </div>
+                    </div>
+            </div>
         </section>
     )
 }
