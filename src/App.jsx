ffd407053c7a9d1a0b06cc29f580a1291c5e35d2
Brunopzn <222485171+brunopzn@users.noreply.github.com>
2026-01-29 18:48:54 -0300

    adicionado registro de reservas e exclusão dos horários registrados

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
 
 import './App.css'
+import { useReducer } from 'react';
 import { Route, Routes } from "react-router-dom";
 import { Link } from 'react-router-dom';
-import Booking from './components/pages/Booking';
 import Confirmation from './components/pages/Confirmation';
 import Home from './components/pages/Home'
 import AboutPage from './components/pages/AboutPage'
 import OnlineMenu from './components/pages/OnlineMenu';
 import Login from './components/pages/Login';
+import Booking from './components/pages/Booking';
 
+
+const initializeTimes = () => ({
+  availableTimes: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
+  reservations: [] 
+});
+
+const updateTimes = (state, action) => {
+  switch (action.type) {
+    case 'updateTime':
+      return initializeTimes();
+    case 'removeTime':
+      return {
+        ...state,
+        availableTimes: state.availableTimes.filter(t => t !== action.payload),
+        reservations: [...state.reservations, action.reservation] 
+      };
+    case 'addReservation':
+      return {
+        ...state,
+        reservations: [...(state.reservations || []), action.payload]
+      };
+    default:
+      return state
+  }
+}
 
 function App() {
-
+  const [state, dispatch] = useReducer(updateTimes, [], initializeTimes);
   return (
     <>
       <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/menu' element={<OnlineMenu/>}/>
-        <Route path='/about' element={<AboutPage/>}/>
-        <Route path='/Confirmation' element={<Confirmation/>}/>
-        <Route path='/Booking' element={<Booking/>}/>
+        <Route path='/Booking' element={<Booking
+          availableTimes={state.availableTimes || []} 
+          reservations={state.reservations || []}
+          dispatch={dispatch} 
+          />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/menu' element={<OnlineMenu />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/Confirmation' element={<Confirmation />} />
         <Route path='/' element={<Home />} />
         <Route path='*' element={
           <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
--- src/components/pages/Booking.jsx
+++ src/components/pages/Booking.jsx
@@ -1,15 +1,108 @@
-import BookingForm from "../BookingForm";
+import style from './Booking.module.css'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../Header';
 
 
-function Booking() {
+function Booking({ availableTimes, reservations, dispatch }) {
+    const today = new Date().toISOString().split('T')[0];
+    const [date, setDate] = useState(today);
+    const [time, setTime] = useState('00:00');
+    const [guests, setGuests] = useState('0');
+    const [occasion, setoccasion] = useState('Birthday');
+    const navigate = useNavigate();
+
+    const handleChangeDate = (e) => {
+        const newDate = e.target.value
+        setDate(newDate)
+        dispatch({ type: updateTime, payload: newDate })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (time === '00:00' || time === '') {
+            alert('You need to select a time.')
+            return
+        } else {
+
+            const reservationDate = {
+                date: date,
+                time: time,
+                occasion: occasion,
+                guests: guests
+            }
+
+            dispatch({
+                type: 'removeTime',
+                payload: time,
+                reservation: reservationDate
+            });
+
+            navigate('/Confirmation', {
+                state: {
+                    date: date,
+                    time: time,
+                    occasion: occasion,
+                    guests: guests
+                }
+            })
+            return
+        }
+
+    }
+
     return (
         <>
-        <Header />
-        <BookingForm/>
-        <Footer />
+            <Header />
+            <section className={style.booking}>
+                <div className={style.formBox}>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="res-date">Choose date</label>
+                        <input type="date" id="res-date" value={date} onChange={handleChangeDate} />
+
+                        <label htmlFor="res-time" >Choose time</label>
+                        <select id="res-time" required value={time} onChange={(e) => setTime(e.target.value)} style={{ borderColor: time == '00:00' ? 'red' : 'initial' }}>
+                            <option value="00:00">Select a time</option>
+                            {availableTimes?.map((t) => {
+                                return <option key={t} value={t}>{t}</option>
+                            })}
+                        </select>
+
+                        <label htmlFor="guests">Number of guests</label>
+                        <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)} style={{ borderColor: guests == '0' ? 'red' : 'initial' }} />
+
+                        <label htmlFor="occasion">Occasion</label>
+                        <select id="occasion" value={occasion} onChange={(e) => setoccasion(e.target.value)}>
+                            <option>Birthday</option>
+                            <option>Anniversary</option>
+                        </select>
+
+                        <input className={style.submit} type="submit" value="Make Your reservation" />
+                    </form>
+                    <h1 className={style.avaliableTimesH1}>Available Times</h1>
+                    <div className={style.disponibleHours}>
+                        {availableTimes?.map((t) => {
+                            return <h2 key={t} value={t}>{t}</h2>
+                        })}
+                    </div>
+                </div>
+                <div className={style.reservationsList}>
+                    <h3>Registered Reservations:</h3>
+                    {reservations.length === 0 ? <p>No reservations made.</p> : (
+                        <ul>
+                            {reservations.map((res, index) => (
+                                <li key={index}>
+                                    <div className={style.reservationsDiv}><h4>Data: </h4>{res.date}</div> <div className={style.reservationsDiv}><h4>Time: </h4> {res.time}</div> <div className={style.reservationsDiv}><h4>Guests: </h4> {res.guests}</div> <div className={style.reservationsDiv}><h4>Occasion: </h4>  ({res.occasion})</div>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </section>
+            <Footer />
         </>
+
     )
 }
 
